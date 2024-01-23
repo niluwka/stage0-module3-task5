80ac4542f26c68aa60e4ad530d72e6fe8c669894
nilufar <ninimamatova0403@gmail.com>
2024-01-23 15:19:55 +0500

    Operators

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        String x = Integer.toString(first) + Integer.toString(second) + Integer.toString(third) ;
+        System.out.println(x);
     }
 }
